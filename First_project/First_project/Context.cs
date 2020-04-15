21638193316e0c62e80e8f3b78a23080df8d0640
Dawid <dawid.medrala.98@gmail.com>
2020-04-15 21:58:13 +0200

    V1

---
+++ First_project/First_project/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_project
+{
+    class Context
+    {
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=DAWIDMDRALA4760;Initial Catalog=simple_order_processing;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
---
+++ First_project/First_project/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var login = textBox1.Text;
+            var haslo = textBox2.Text;
+
+            
+            string rules = "SELECT * FROM[simple_order_processing].[dbo].[Pracownik] Where login = '" + login + "' AND password = '"+haslo+"'";
+
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DAWIDMDRALA4760;Initial Catalog=simple_order_processing;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            sqlcon.Open();
+
+            SqlCommand sqlcmd = new SqlCommand(rules, sqlcon);
+
+            SqlDataReader dr = sqlcmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+
+                var valueRules = (dr["rules"].ToString());
+
+
+
+                var admin = "1";
+                admin.ToString();
+                var pracownik = 1;
+                //jan123 haslo512
+
+
+                if (valueRules = admin)
+                {
+                    this.Hide();
+                    var FormAdmin = new FormAdmin();
+                    FormAdmin.Closed += (s, args) => this.Close();
+                    FormAdmin.Show();
+                }
+                else if (valueRules = pracownik)
+                {
+
+                    this.Hide();
+                    var FormAdmin = new FormAdmin();
+                    FormAdmin.Closed += (s, args) => this.Close();
+                    FormAdmin.Show();
+                }
+            }
+            
+        }
+    }
+}
