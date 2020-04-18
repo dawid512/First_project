382ac6a5644a833dc588f835c6253a7c3cba5a15
Dawid <dawid.medrala.98@gmail.com>
2020-04-19 01:22:10 +0200

    v9

---
+++ First_project/First_project/Pracownicy.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace First_project
+{
+    partial class Pracownicy
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.usuńToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1336, 846);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuńToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(301, 86);
+            // 
+            // usuńToolStripMenuItem
+            // 
+            this.usuńToolStripMenuItem.Name = "usuńToolStripMenuItem";
+            this.usuńToolStripMenuItem.Size = new System.Drawing.Size(300, 38);
+            this.usuńToolStripMenuItem.Text = "Usuń";
+            // 
+            // Pracownicy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Pracownicy";
+            this.Size = new System.Drawing.Size(1410, 901);
+            this.Load += new System.EventHandler(this.Pracownicy_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usuńToolStripMenuItem;
+    }
+}
---
+++ First_project/First_project/Pracownicy.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace First_project
+{
+    public partial class Pracownicy : UserControl
+    {
+        public Pracownicy()
+        {
+            InitializeComponent();
+        }
+        public static SqlConnection connection = new SqlConnection(Context.sqlconnection);
+        private void Pracownicy_Load(object sender, EventArgs e)
+        {
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            connection.Open();
+
+            fill_grid();
+        }
+
+        public void fill_grid()
+        {
+
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select* From[simple_order_processing].[dbo].[Pracownik]";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        string id;
+        int id1;
+        int delete_id;
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+            id = dataGridView1.Rows[e.RowIndex].Cells["id_Pracownika"].Value.ToString();
+
+            if (id == "")
+            {
+                id1 = 0;
+            }
+            else
+            {
+                id1 = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["id_pracownika"].Value.ToString());
+            }
+
+            if (id1 == 0)
+            {
+            }
+            else
+            {
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update [simple_order_processing].[dbo].[Pracownik] set Imie='" + dataGridView1.Rows[e.RowIndex].Cells["Imie"].Value.ToString() + "',Nazwisko='" + dataGridView1.Rows[e.RowIndex].Cells["Nazwisko"].Value.ToString() + "',telefon='" + dataGridView1.Rows[e.RowIndex].Cells["telefon"].Value.ToString() + "',[e-mail]='" + dataGridView1.Rows[e.RowIndex].Cells["e-mail"].Value.ToString() + "',adres='" + dataGridView1.Rows[e.RowIndex].Cells//["adres"].Value.ToString() + "' Where id_klienta=" + id + ";";
+                cmd.ExecuteNonQuery();
+                fill_grid();
+            }
+
+
+
+
+        }
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from [simple_order_processing].[dbo].[Praconik] where id_pracownika=" + delete_id + ";";
+            cmd.ExecuteNonQuery();
+            fill_grid();
+        }
+
+        private void dataGridView1_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                delete_id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["id_pracownika"].Value.ToString());
+                this.contextMenuStrip1.Show(this.dataGridView1, e.Location);
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+    }
+}
